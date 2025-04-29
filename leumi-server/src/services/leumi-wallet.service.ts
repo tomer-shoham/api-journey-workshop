aa89ccc8a82b02db3483f86b033ac6862f0fe2a2
Tomer Shoham <tshoham@fireblocks.com>
2025-04-29 19:26:17 +0300

    feat: updateLeumiWallet functionality

--- leumi-server/src/services/leumi-wallet.service.ts
+++ leumi-server/src/services/leumi-wallet.service.ts
@@ -1,6 +1,7 @@
 import { LeumiWallet } from "../db/models/LeumiWallet.model";
 import { FireblocksService } from "./fireblocks/fireblocks.service";
 import { User } from "../db/models/User.model";
+import { LeumiWalletType } from "../types/leumi-wallet.types";
 
 export class LeumiWalletService {
   constructor(private readonly fireblocksService: FireblocksService) {}
@@ -15,7 +16,20 @@ export class LeumiWalletService {
       include: [User]
     });
   }
-  public async updateLeumiWallet() {
-    return {};
+  public async updateLeumiWallet(leumiWalletId: string, fieldsToUpdate: Partial<LeumiWalletType>) {
+    try {
+      const leumiWallet = await LeumiWallet.findOne({ where: { id: leumiWalletId } });
+      if (!leumiWallet) {
+        throw new Error("Leumi Wallet not found");
+      }
+
+      const updatedWallet = await leumiWallet.update(fieldsToUpdate);
+
+      console.log("Leumi Wallet updated successfully:", updatedWallet);
+      return updatedWallet.toJSON();
+    } catch (error) {
+      console.error("Error updating Leumi Wallet:", error);
+      throw new Error("Failed to update Leumi Wallet");
+    }
   }
 }
