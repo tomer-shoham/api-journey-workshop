bb6d5b545159206794cca227b1b6746d84445352
Tomer Shoham <tshoham@fireblocks.com>
2025-04-29 19:24:22 +0300

    feat: getTransactions functionality

--- leumi-server/src/services/transaction.service.ts
+++ leumi-server/src/services/transaction.service.ts
@@ -1,7 +1,20 @@
+import { Transaction } from "../db/models/Transaction.model";
+
 import { FireblocksService } from "./fireblocks/fireblocks.service";
+
 export class TransactionService {
   constructor(private readonly fireblocksService: FireblocksService) {}
-  public async getTransactions() {
-    return [];
+  public async getTransactions(leumiWalletId: string) {
+    try {
+      const transactions = await Transaction.findAll({
+        where: {
+          leumiWalletId: leumiWalletId
+        }
+      });
+      console.log("transactions", transactions);
+      return transactions.map(transaction => transaction.toJSON());
+    } catch (err) {
+      throw new Error(`Error fetching TXs - ${(err as any).message || JSON.stringify(err)}`);
+    }
   }
 }
