1096b0a278abb2e399a470e9bdfc750a638926eb
Tomer Shoham <tshoham@fireblocks.com>
2025-05-04 13:49:55 +0300

    feat: handleWebhook func + added assetSerivice usage

--- leumi-server/src/services/webhook.service.ts
+++ leumi-server/src/services/webhook.service.ts
@@ -1,5 +1,38 @@
+import { TransactionResponse } from "@fireblocks/ts-sdk";
 import { FireblocksService } from "./fireblocks/fireblocks.service";
+import { LeumiWallet } from "../db/models/LeumiWallet.model";
+import { AssetService } from "./asset.service";
+import { Asset } from "../db/models/Asset.model";
 export class WebhookService {
-  constructor(private readonly fireblocksService: FireblocksService) {}
-  public async handleWebhook() {}
+  constructor(
+    private readonly fireblocksService: FireblocksService,
+    private readonly assetService: AssetService
+  ) {}
+  public async handleWebhook(type: string, tx: TransactionResponse) {
+    try {
+      if (
+        type === "TRANSACTION_STATUS_UPDATED" &&
+        tx.status === "COMPLETED" &&
+        tx.amountInfo &&
+        tx.amountInfo?.amount
+      ) {
+        const vaultId = tx.destination?.id;
+        const assetId = tx.assetId;
+        const amount = parseFloat(tx.amountInfo.amount);
+        console.log("web hook service", vaultId, assetId, amount);
+        const asset = await Asset.findOne({ where: { vaultId } });
+        if (!asset || !assetId) {
+          throw new Error("Error fetching asset data");
+        }
+        const leumiWallet = await LeumiWallet.findByPk(asset.leumiWalletId);
+        if (!leumiWallet) {
+          throw new Error("Error fetching LW data");
+        }
+        await this.assetService.deposit(leumiWallet.id, assetId, amount);
+        console.log("handleWebhook - done");
+      }
+    } catch (err) {
+      throw new Error(`Error handling webhook - ${(err as any).message || JSON.stringify(err)}`);
+    }
+  }
 }
