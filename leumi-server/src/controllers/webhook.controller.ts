17a777102f3c1377f606f00b326d0a6753d71d9b
Tomer Shoham <tshoham@fireblocks.com>
2025-05-04 13:49:34 +0300

    feat: added params to whservice func (handleWebhook)

--- leumi-server/src/controllers/webhook.controller.ts
+++ leumi-server/src/controllers/webhook.controller.ts
@@ -3,7 +3,14 @@ import { Request, Response } from "express";
 export class WebhookController {
   constructor(private readonly webhookService: WebhookService) {}
   public async handleWebhook(req: Request, res: Response) {
-    await this.webhookService.handleWebhook();
-    res.sendStatus(200);
+    try {
+      console.log("webhook req", req.body);
+      const { type, data } = req.body;
+      await this.webhookService.handleWebhook(type, data);
+      res.sendStatus(200);
+    } catch (e) {
+      console.error(e);
+      res.sendStatus(400);
+    }
   }
 }
