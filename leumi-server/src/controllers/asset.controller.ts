e2f4bb6d0cb80d1a85bbe9be8bafa9a0122291fa
Tomer Shoham <tshoham@fireblocks.com>
2025-05-06 14:00:44 +0300

    feat: getVaultAccount

--- leumi-server/src/controllers/asset.controller.ts
+++ leumi-server/src/controllers/asset.controller.ts
@@ -3,6 +3,23 @@ import { Request, Response } from "express";
 
 export class AssetController {
   constructor(private readonly assetService: AssetService) {}
+
+  public async getVaultAccount(req: Request, res: Response) {
+    try {
+      console.log("getVaultAccount");
+      const vaultAccountId = String(req.params.vaultAccountId);
+      console.log("getVaultAccount", vaultAccountId);
+      const vaultAccount = await this.assetService.getFbVaultAccount(vaultAccountId);
+      res.status(200).json(vaultAccount);
+    } catch (err: any) {
+      console.error("Error in getVaultAccount:", err);
+      res.status(500).json({
+        message: "Failed to fetch vault account.",
+        error: err.message || "An unexpected error occurred"
+      });
+    }
+  }
+
   public async getAssets(req: Request, res: Response) {
     try {
       const leumiWalletId = req.params.leumiWalletId;
@@ -49,7 +66,9 @@ export class AssetController {
   public async withdrawal(req: Request, res: Response) {
     try {
       const { leumiWalletId, assetId } = req.params;
-      const { amount, address } = req.body;
+      const address = req.body.address;
+      const amount = Number(req.body.amount);
+
       await this.assetService.withdrawal(leumiWalletId, assetId, amount, address);
       res.sendStatus(200);
     } catch (err: any) {
@@ -64,7 +83,7 @@ export class AssetController {
   public async deposit(req: Request, res: Response) {
     try {
       const { leumiWalletId, assetId } = req.params;
-      const { amount } = req.body;
+      const amount = Number(req.body.amount);
       await this.assetService.deposit(leumiWalletId, assetId, amount);
       res.sendStatus(200);
     } catch (err: any) {
@@ -79,7 +98,7 @@ export class AssetController {
     try {
       const leumiWalletId = req.params.leumiWalletId;
       const assetId = req.params.assetId as Assets;
-      const { amount } = req.body;
+      const amount = Number(req.body.amount);
       console.log(amount);
       await this.assetService.buy(leumiWalletId, assetId, amount);
       res.sendStatus(200);
